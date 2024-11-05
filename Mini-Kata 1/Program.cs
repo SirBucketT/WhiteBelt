d8a1a52e5f9b7e21a19eee1d3d8a7dc012e5868f
SirBucketT <tkpriva@icloud.com>
2024-11-05 10:35:02 +0100

    Mini-Kata 1 - Variable Practice
    completed assignment

---
+++ Mini-Kata 1/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Give me a name");
+        string name = Console.ReadLine();
+        int healthPoints = 100;
+        float attackPower = 15.5f;
+        bool isParalyzed = true;
+        double currency = 24.75;
+        
+        Console.WriteLine($"Character Name: {name} \n " +
+                          $"Health Points: {healthPoints} \n " +
+                          $"Attack Power: {attackPower} \n " +
+                          $"Is Paralyzed: {isParalyzed} \n " +
+                          $"Gold Coins: {currency}");
+    }
+}
